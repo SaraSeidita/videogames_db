e7192dc0b7955f80bf334bf97dfa7fecacf16eca
Sara Seidita <61561159+SaraSeidita@users.noreply.github.com>
2026-03-16 14:32:05 +0100

    Rename tabella_utente.sql to db/tabella_utente.sql

--- db/tabella_utente.sql
+++ db/tabella_utente.sql
@@ -6,6 +6,6 @@ CREATE TABLE Auth (
 
 
 ALTER TABLE Auth ALTER COLUMN Username NVARCHAR(50);
-ALTER TABLE Auth ALTER COLUMN Pw NVARCHAR(255); -- Più spazio per password sicure
+ALTER TABLE Auth ALTER COLUMN Pw NVARCHAR(255); -- PiÃ¹ spazio per password sicure
 ALTER TABLE Auth ADD ProfilePicUrl NVARCHAR(500);
 select * from auth
