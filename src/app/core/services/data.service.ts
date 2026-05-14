92da41a146386c051afcd2b7e839d943d2d23e4c
Sara Six <61561159+Rosmerade@users.noreply.github.com>
2026-05-14 15:20:23 +0200

    struttura demo

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 
+
+// Questo servizio è un placeholder per le chiamate al backend per il recupero dei dati, 
+// come la lista dei videogiochi, i dettagli di un videogioco, ecc.
+
 @Injectable({
   providedIn: 'root',
 })
--- src/app/shared/models/record.ts
+++ src/app/shared/models/record.ts
@@ -1,2 +1,4 @@
+// modello per rappresentare lista dei videogiochi e i dettagli di un videogioco, da utilizzare nei componenti record-list e record-detail
+
 export interface Record {
 }
